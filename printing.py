d9254da8366b462a4c2ec8387a9ea3cb5f50a602
VijiVenkat45 <166313882+VijiVenkat45@users.noreply.github.com>
2024-04-08 00:50:56 +0800

    Delete printing.py
    To connect from pycharm, i am deleting this

--- printing.py
+++
@@ -1,8 +0,0 @@
-# Print function to handle the quotes within the string itself
-print("It is a \"beautiful\" day!")
-'''print function to handle the quotes within the string itself without using escape'''
-print('It is a "beautiful" day!')
-"""print function to concatenate two strings including new line"""
-print("It is a \"beautiful\" day!\nIt is a 'beautiful' day!")
-"""print function to concatenate two strings using '+' and a new line"""
-print("Hello World"+'\nIt is a "beautiful" day')
